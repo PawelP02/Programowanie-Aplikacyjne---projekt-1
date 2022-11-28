e082ceeb353554d8cb1bfe59b26ecee90a5204ca
PawelP02 <103063714+PawelP02@users.noreply.github.com>
2022-11-28 17:53:55 +0100

    Add files via upload

--- Konwerter3/Form1.Designer.cs
+++ Konwerter3/Form1.Designer.cs
@@ -40,17 +40,20 @@
             this.button1 = new System.Windows.Forms.Button();
             this.nowa1 = new System.Windows.Forms.TextBox();
             this.nowa2 = new System.Windows.Forms.TextBox();
-            this.przelicznik = new System.Windows.Forms.NumericUpDown();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.przelicznik)).BeginInit();
+            this.rodzaj = new System.Windows.Forms.ComboBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.licznik = new System.Windows.Forms.NumericUpDown();
+            this.rodzaj2 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.licznik)).BeginInit();
             this.SuspendLayout();
             // 
             // Przycisk
             // 
-            this.Przycisk.Location = new System.Drawing.Point(116, 190);
+            this.Przycisk.Location = new System.Drawing.Point(108, 274);
             this.Przycisk.Name = "Przycisk";
             this.Przycisk.Size = new System.Drawing.Size(124, 37);
             this.Przycisk.TabIndex = 0;
@@ -61,7 +64,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(154, 289);
+            this.label1.Location = new System.Drawing.Point(147, 319);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(40, 15);
             this.label1.TabIndex = 1;
@@ -70,7 +73,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 20);
+            this.label2.Location = new System.Drawing.Point(12, 82);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(112, 15);
             this.label2.TabIndex = 2;
@@ -79,7 +82,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(248, 20);
+            this.label3.Location = new System.Drawing.Point(240, 82);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(112, 15);
             this.label3.TabIndex = 3;
@@ -88,7 +91,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(138, 102);
+            this.label4.Location = new System.Drawing.Point(130, 227);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(82, 15);
             this.label4.TabIndex = 4;
@@ -97,30 +100,23 @@
             // wynik
             // 
             this.wynik.Enabled = false;
-            this.wynik.Location = new System.Drawing.Point(97, 307);
+            this.wynik.Location = new System.Drawing.Point(90, 337);
             this.wynik.Name = "wynik";
             this.wynik.Size = new System.Drawing.Size(160, 23);
             this.wynik.TabIndex = 5;
             // 
             // wartosc
             // 
-            this.wartosc.Location = new System.Drawing.Point(129, 120);
+            this.wartosc.Location = new System.Drawing.Point(90, 245);
             this.wartosc.Name = "wartosc";
-            this.wartosc.Size = new System.Drawing.Size(100, 23);
+            this.wartosc.Size = new System.Drawing.Size(160, 23);
             this.wartosc.TabIndex = 6;
             // 
             // jednostka1
             // 
             this.jednostka1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.jednostka1.FormattingEnabled = true;
-            this.jednostka1.Items.AddRange(new object[] {
-            "m",
-            "km",
-            "g",
-            "kg",
-            "ml",
-            "l"});
-            this.jednostka1.Location = new System.Drawing.Point(39, 38);
+            this.jednostka1.Location = new System.Drawing.Point(12, 102);
             this.jednostka1.Name = "jednostka1";
             this.jednostka1.Size = new System.Drawing.Size(67, 23);
             this.jednostka1.TabIndex = 7;
@@ -129,14 +125,7 @@
             // 
             this.jednostka2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.jednostka2.FormattingEnabled = true;
-            this.jednostka2.Items.AddRange(new object[] {
-            "km",
-            "m",
-            "kg",
-            "g",
-            "l",
-            "ml"});
-            this.jednostka2.Location = new System.Drawing.Point(263, 38);
+            this.jednostka2.Location = new System.Drawing.Point(240, 102);
             this.jednostka2.Name = "jednostka2";
             this.jednostka2.Size = new System.Drawing.Size(67, 23);
             this.jednostka2.TabIndex = 8;
@@ -165,22 +154,10 @@
             this.nowa2.Size = new System.Drawing.Size(100, 23);
             this.nowa2.TabIndex = 11;
             // 
-            // przelicznik
-            // 
-            this.przelicznik.Location = new System.Drawing.Point(494, 252);
-            this.przelicznik.Maximum = new decimal(new int[] {
-            10000,
-            0,
-            0,
-            0});
-            this.przelicznik.Name = "przelicznik";
-            this.przelicznik.Size = new System.Drawing.Size(100, 23);
-            this.przelicznik.TabIndex = 12;
-            // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(484, 64);
+            this.label5.Location = new System.Drawing.Point(485, 20);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(120, 15);
             this.label5.TabIndex = 13;
@@ -204,25 +181,72 @@
             this.label7.TabIndex = 15;
             this.label7.Text = "Nowa jednostka 2";
             // 
-            // label8
+            // rodzaj
             // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(494, 234);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(62, 15);
-            this.label8.TabIndex = 16;
-            this.label8.Text = "Przelicznik";
+            this.rodzaj.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.rodzaj.FormattingEnabled = true;
+            this.rodzaj.Items.AddRange(new object[] {
+            "Odległość",
+            "Waga",
+            "Objętość"});
+            this.rodzaj.Location = new System.Drawing.Point(12, 38);
+            this.rodzaj.Name = "rodzaj";
+            this.rodzaj.Size = new System.Drawing.Size(85, 23);
+            this.rodzaj.TabIndex = 20;
+            this.rodzaj.SelectedIndexChanged += new System.EventHandler(this.Rodzaj_SelectedIndexChanged);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(12, 20);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(138, 15);
+            this.label10.TabIndex = 21;
+            this.label10.Text = "Wybierz rodzaj jednostek";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(57, 163);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(221, 15);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "Wpisz przelicznik dla własnych jednostek";
+            // 
+            // licznik
+            // 
+            this.licznik.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.licznik.Location = new System.Drawing.Point(108, 181);
+            this.licznik.Name = "licznik";
+            this.licznik.Size = new System.Drawing.Size(120, 23);
+            this.licznik.TabIndex = 19;
+            // 
+            // rodzaj2
+            // 
+            this.rodzaj2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.rodzaj2.FormattingEnabled = true;
+            this.rodzaj2.Items.AddRange(new object[] {
+            "Odległość",
+            "Waga",
+            "Objętość"});
+            this.rodzaj2.Location = new System.Drawing.Point(494, 38);
+            this.rodzaj2.Name = "rodzaj2";
+            this.rodzaj2.Size = new System.Drawing.Size(86, 23);
+            this.rodzaj2.TabIndex = 22;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(706, 408);
-            this.Controls.Add(this.label8);
+            this.Controls.Add(this.rodzaj2);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.rodzaj);
+            this.Controls.Add(this.licznik);
+            this.Controls.Add(this.label9);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.przelicznik);
             this.Controls.Add(this.nowa2);
             this.Controls.Add(this.nowa1);
             this.Controls.Add(this.button1);
@@ -237,7 +261,7 @@
             this.Controls.Add(this.Przycisk);
             this.Name = "Form1";
             this.Text = "Konwerter jednostek";
-            ((System.ComponentModel.ISupportInitialize)(this.przelicznik)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.licznik)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -257,10 +281,13 @@
         private Button button1;
         private TextBox nowa1;
         private TextBox nowa2;
-        private NumericUpDown przelicznik;
         private Label label5;
         private Label label6;
         private Label label7;
-        private Label label8;
+        private ComboBox rodzaj;
+        private Label label10;
+        private Label label9;
+        private NumericUpDown licznik;
+        private ComboBox rodzaj2;
     }
 }
--- Konwerter3/Form1.cs
+++ Konwerter3/Form1.cs
@@ -11,46 +11,117 @@ namespace Konwerter3
         public Form1()
         {
             InitializeComponent();
-           
+
 
         }
-
-        private void Przycisk_Click(object sender, EventArgs e)
+        private void Form1_Load(object sender, EventArgs e)
         {
-            if(jednostka1.SelectedItem == "m" && jednostka2.SelectedItem == "km")
+
+        }
+
+        private void Rodzaj_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (rodzaj.SelectedItem == "Odleg³oœæ")
             {
-                wynik.Text = (double.Parse(wartosc.Text) * 0.001).ToString();
-            }         
-            if (jednostka1.SelectedItem == "g" && jednostka2.SelectedItem == "kg")
-            {
-                wynik.Text = (double.Parse(wartosc.Text) * 0.001).ToString();
-            }            
-            if (jednostka1.SelectedItem == "ml" && jednostka2.SelectedItem == "l")
-            {
-                wynik.Text = (double.Parse(wartosc.Text) * 0.001).ToString();
+                jednostka1.Items.Clear();
+                jednostka2.Items.Clear();
+                jednostka1.Items.Add("m");
+                jednostka1.Items.Add("km");
+                jednostka2.Items.Add("m");
+                jednostka2.Items.Add("km");
+
             }
-            if (jednostka1.SelectedItem == "km" && jednostka2.SelectedItem == "m")
+            if (rodzaj.SelectedItem == "Waga")
             {
-                wynik.Text = (double.Parse(wartosc.Text) * 1000).ToString();
+                jednostka1.Items.Clear();
+                jednostka2.Items.Clear();
+                jednostka1.Items.Add("g");
+                jednostka1.Items.Add("kg");
+                jednostka2.Items.Add("g");
+                jednostka2.Items.Add("kg");
+
             }
-            if (jednostka1.SelectedItem == "kg" && jednostka2.SelectedItem == "g")
+            if (rodzaj.SelectedItem == "Objêtoœæ")
             {
-                wynik.Text = (double.Parse(wartosc.Text) * 1000).ToString();
-            }
-            if (jednostka1.SelectedItem == "l" && jednostka2.SelectedItem == "ml")
-            {
-                wynik.Text = (double.Parse(wartosc.Text) * 1000).ToString();
+                jednostka1.Items.Clear();
+                jednostka2.Items.Clear();
+                jednostka1.Items.Add("ml");
+                jednostka1.Items.Add("l");
+                jednostka2.Items.Add("ml");
+                jednostka2.Items.Add("l");
+
             }
         }
+    private void Przycisk_Click(object sender, EventArgs e)
+        {
+            
+            if (rodzaj.Text != "")
+            {
+                if (rodzaj.SelectedItem == "Odleg³oœæ")
+                {
+                    
+                    if (jednostka1.SelectedItem == "m" && jednostka2.SelectedItem == "km")
+                    {
+                        wynik.Text = (double.Parse(wartosc.Text) * 0.001).ToString();
+                    }
+                    if (jednostka1.SelectedItem == "km" && jednostka2.SelectedItem == "m")
+                    {
+                        wynik.Text = (double.Parse(wartosc.Text) * 1000).ToString();
+                    }
+                    
+                }
+                if (rodzaj.SelectedItem == "Waga")
+                {
+                    if (jednostka1.SelectedItem == "g" && jednostka2.SelectedItem == "kg")
+                    {
+                        wynik.Text = (double.Parse(wartosc.Text) * 0.001).ToString();
+                    }
+                    if (jednostka1.SelectedItem == "kg" && jednostka2.SelectedItem == "g")
+                    {
+                        wynik.Text = (double.Parse(wartosc.Text) * 1000).ToString();
+                    }
+                }
+                if (rodzaj.SelectedItem == "Objêtoœæ")
+                {
+                    if (jednostka1.SelectedItem == "ml" && jednostka2.SelectedItem == "l")
+                    {
+                        wynik.Text = (double.Parse(wartosc.Text) * 0.001).ToString();
+                    }
+                    if (jednostka1.SelectedItem == "l" && jednostka2.SelectedItem == "ml")
+                    {
+                        wynik.Text = (double.Parse(wartosc.Text) * 1000).ToString();
+                    }
+                }
+                wynik.Text = (int.Parse(wartosc.Text) * licznik.Value).ToString();
+                licznik.Value = licznik.Minimum;
+            }
+        }
+
+
 
         private void button1_Click(object sender, EventArgs e)
         {
-            jednostka1.Items.Add(nowa1.Text);
-            jednostka2.Items.Add(nowa2.Text);
-            
-            nowa1.Clear();
-            nowa2.Clear();
-            
+            if (rodzaj.SelectedItem == "Odleg³oœæ" && rodzaj2.SelectedItem == "Odleg³oœæ")
+            {
+                jednostka1.Items.Add(nowa1.Text);
+                jednostka2.Items.Add(nowa2.Text);
+                nowa1.Clear();
+                nowa2.Clear();
+            }
+            if (rodzaj.SelectedItem == "Waga" && rodzaj2.SelectedItem == "Waga")
+            {
+                jednostka1.Items.Add(nowa1.Text);
+                jednostka2.Items.Add(nowa2.Text);
+                nowa1.Clear();
+                nowa2.Clear();
+            }
+            if (rodzaj.SelectedItem == "Objêtoœæ" && rodzaj2.SelectedItem == "Objêtoœæ")
+            {
+                jednostka1.Items.Add(nowa1.Text);
+                jednostka2.Items.Add(nowa2.Text);
+                nowa1.Clear();
+                nowa2.Clear();
+            }
         }
     }
 }
